62940dc965b205eed7d9247e999047967c6588fa
averageodds <129436681+averageodds@users.noreply.github.com>
2023-03-31 21:34:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-window.onload = function() {
-  reset();
-};
-
 function calculate() {
   let odds = [];
   let stakes = [];
   let oddsWithComms = [];
 
-  for (let i = 1; i <= 10; i++) {
+  const table = document.getElementsByTagName('table')[0];
+  const rows = table.rows.length;
+
+  for (let i = 1; i < rows; i++) {
     let oddsField = document.getElementById("odds" + i);
     let stakeField = document.getElementById("stake" + i);
     let commissionField = document.getElementById("commission" + i);
 
-    if (oddsField.value && stakeField.value) {
+    if (oddsField && stakeField && commissionField) {
       let oddsValue = parseFloat(oddsField.value);
       let stakeValue = parseFloat(stakeField.value.replace(/[^\d.-]/g, ""));
       let commissionValue = parseFloat(commissionField.value.replace(/[^\d.-]/g, ""));
@@ -28,6 +27,8 @@ function calculate() {
         odds.push(oddsValue);
         stakes.push(stakeValue);
         oddsWithComms.push(oddsWithComm);
+
+        document.getElementById("oddsWithComm" + i).innerHTML = oddsWithComm.toFixed(2);
       }
     }
   }
@@ -44,11 +45,11 @@ function calculate() {
 
   document.getElementById("averageOdds").innerHTML = averageOdds.toFixed(2);
   document.getElementById("totalStake").innerHTML = "£" + totalStake.toLocaleString("en-GB", {minimumFractionDigits: 2, maximumFractionDigits: 2});
-
-  for (let i = 0; i < oddsWithComms.length; i++) {
-    document.getElementById("oddsWithComm" + (i+1)).innerHTML = oddsWithComms[i].toFixed(2);
-  }
 }
+
+
+
+
 
 function reset() {
   for (let i = 1; i <= 10; i++) {
@@ -59,4 +60,54 @@ function reset() {
   }
   document.getElementById("averageOdds").innerHTML = "0.00";
   document.getElementById("totalStake").innerHTML = "£0.00";
+
+  const select = document.getElementById('rows');
+  select.value = '10';
+  select.dispatchEvent(new Event('change'));
 }
+
+// Get the select element and table
+const select = document.getElementById('rows');
+const table = document.getElementsByTagName('table')[0];
+
+// Store the previous value of rows
+let prevRows = select.value;
+
+// Add an event listener to the select element
+select.addEventListener('change', function() {
+  // Get the selected value
+  const rows = select.value;
+
+  // Add or remove rows from the table based on the difference between the current and previous values
+  if (rows > prevRows) {
+    for (let i = prevRows; i < rows; i++) {
+      const row = table.insertRow(-1);
+      const index = table.rows.length - 2;
+      row.innerHTML = `
+        <td>${index + 1}</td>
+        <td class="bookmaker"><input type="text"></td>
+        <td class="stake-input"><span class="currency">£</span><input type="number" step="0.01" id="stake${index + 1}" oninput="calculate()" style="width: 80%;"></td>
+        <td class="odds-input"><input type="number" step="0.01" id="odds${index + 1}" oninput="calculate()" style="width: 80%;"></td>
+        <td class="commission-input"><input type="number" step="1" id="commission${index + 1}" value="0" oninput="calculate()" style="width: 80%;"><span class="percentsign">%</span></td>
+        <td id="oddsWithComm${i+1}"><span>0.00</span></td>
+      `;
+    }
+  } else {
+    for (let i = prevRows - 1; i >= rows; i--) {
+      table.deleteRow(i + 1);
+    }
+  }
+
+  // Update the previous value of rows
+  prevRows = rows;
+
+  // Update the row numbers
+  const rowsCells = table.getElementsByTagName("td");
+  for (let i = 0; i < rowsCells.length; i++) {
+    if (rowsCells[i].textContent.match(/^\d+$/)) {
+      rowsCells[i].textContent = i / 6 + 1;
+    }
+  }
+});
+
+
